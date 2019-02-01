6ce5db20352815d060c3424bab7d4b2d450a8a6d
xianfengting <1142631421@qq.com>
2019-02-01 21:50:43 +0800

    Realize the Kotlin code in AppMainActivity.kt (Part 1, unfinished)

--- app/src/main/java/com/src_resources/picturetoasciiconverter/AppMainActivity.kt
+++ app/src/main/java/com/src_resources/picturetoasciiconverter/AppMainActivity.kt
@@ -2,11 +2,30 @@ package com.src_resources.picturetoasciiconverter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class AppMainActivity : AppCompatActivity() {
+
+    private lateinit var convertButton: Button
+    private lateinit var convertingPictureEditText: EditText
+    private lateinit var savingPictureEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_main)
+
+        convertButton = findViewById(R.id.convertButton)
+        convertButton.setOnClickListener {
+            executePythonForConvertingPicture(convertingPictureEditText.text.toString(),
+                    savingPictureEditText.text.toString())
+        }
+
+        convertingPictureEditText = findViewById(R.id.convertingPictureEditText)
+        savingPictureEditText = findViewById(R.id.savingPictureEditText)
+    }
+
+    private fun executePythonForConvertingPicture(convertingPicturePath: String, savingPictureEditText: String) {
+        // TODO
     }
 }
