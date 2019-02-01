ed2bf98cc2d1bdf84ef766a8d890572630fe34cd
xianfengting <1142631421@qq.com>
2019-02-01 19:34:50 +0800

    The first commit for the files of the project.

---
+++ app/src/main/java/com/src_resources/picturetoasciiconverter/AppMainActivity.kt
@@ -0,0 +1,12 @@
+package com.src_resources.picturetoasciiconverter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AppMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_main)
+    }
+}
