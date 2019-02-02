5078235e79698e63779f6721090c367e9cf9f3b2
xianfengting <1142631421@qq.com>
2019-02-02 12:34:57 +0800

    Added the CLE library (for supporting Python) and some code to copy the python files to the cache directory of the APP.

--- app/src/main/java/com/src_resources/picturetoasciiconverter/AppMainActivity.kt
+++ app/src/main/java/com/src_resources/picturetoasciiconverter/AppMainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ListAdapter
 import android.widget.ListView
+import java.io.File
 
 class AppMainActivity : AppCompatActivity() {
 
@@ -35,15 +36,24 @@ class AppMainActivity : AppCompatActivity() {
         // Only for debugging
         if (BuildConfig.DEBUG) mLogStringList.add("Test Log 1")
         mLogMutableStringListAdapter = MutableStringListAdapter(this, mLogStringList)
+        logListView.adapter = mLogMutableStringListAdapter
         // Only for debugging
         if (BuildConfig.DEBUG) mLogStringList.add("Test Log 2")
     }
 
     private fun executePythonForConvertingPicture(convertingPicturePath: String, savingPictureEditText: String) {
         // 获取到 picture-to-ascii.py 文件的 InputStream 。
+        mLogStringList.add("正在准备从应用程序中加载 Python 脚本到缓存目录...")
         val pythonFileInputStream = assets.open("picture-to-ascii.py")
         // 读取 picture-to-ascii.py 中的内容。
-        val pythonFileText = readWholeStringFromInputStream(pythonFileInputStream)
+//        val pythonFileText = readWholeStringFromInputStream(pythonFileInputStream)
+        // 创建一个 File 对象，表示要复制到的 picture-to-ascii.py 的路径。
+        val pythonFileCacheFileObj = File(cacheDir, "picture-to-ascii.py")
+        // 如果文件不存在，创建文件。
+        if (!pythonFileCacheFileObj.exists()) pythonFileCacheFileObj.createNewFile()
+        // 复制文件。
+        mLogStringList.add("正在从应用程序中加载 Python 脚本到缓存目录...")
+        copyFromInputStreamToFile(pythonFileInputStream, pythonFileCacheFileObj)
         // TODO ：执行该 python 代码。
     }
 }
--- app/src/main/java/com/src_resources/picturetoasciiconverter/IOHelper.kt
+++ app/src/main/java/com/src_resources/picturetoasciiconverter/IOHelper.kt
@@ -1,6 +1,8 @@
 package com.src_resources.picturetoasciiconverter
 
+import java.io.File
 import java.io.InputStream
+import java.io.OutputStream
 import java.nio.charset.Charset
 
 fun readWholeStringFromInputStream(inputStream: InputStream,
@@ -17,4 +19,47 @@ fun readWholeStringFromInputStream(inputStream: InputStream,
     val str = String(buffer, encoding)
     // 返回该字符串。
     return str
+}
+
+fun copyFile(targetFile: File, targetDir: File) {
+    if (!targetFile.exists())
+        throw IllegalArgumentException("The argument targetFile does not exist.")
+    if (!targetDir.exists())
+        throw IllegalArgumentException("The argument targetDir does not exist.")
+    if (!targetFile.isFile)
+        throw IllegalArgumentException("The argument targetFile does not mean a file.")
+    if (!targetDir.isDirectory)
+        throw IllegalArgumentException("The argument targetDir does not mean a directory.")
+}
+
+fun copyFile(targetFile: File, targetDir: String) {
+    copyFile(targetFile, File(targetDir))
+}
+
+fun copyFromInputStreamToOutputStream(targetInputStream: InputStream, targetOutputStream: OutputStream) {
+    // 获取到 InputStream 可以读取的字节数。
+    val size = targetInputStream.available()
+    // 创建一个字节数组用于保存读到的字节。
+    val buffer = ByteArray(size)
+    // 从 InputStream 读字节。
+    targetInputStream.read(buffer)
+    // 关闭 InputStream 。
+    targetInputStream.close()
+    // 将读到的字节写入 OutputStream 。
+    targetOutputStream.write(buffer)
+    // 关闭 OutputStream 。
+    targetOutputStream.close()
+}
+
+fun copyFromInputStreamToFile(targetInputStream: InputStream, targetFile: File) {
+    if (!targetFile.exists())
+        throw IllegalArgumentException("The argument targetFile does not exist.")
+    if (!targetFile.isFile)
+        throw IllegalArgumentException("The argument targetFile does not mean a file.")
+
+    copyFromInputStreamToOutputStream(targetInputStream, targetFile.outputStream())
+}
+
+fun copyFromInputStreamToFile(targetInputStream: InputStream, targetFile: String) {
+    copyFromInputStreamToFile(targetInputStream, File(targetFile))
 }
